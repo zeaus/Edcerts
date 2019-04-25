52b4a44721ce5a0fd8f2aae945422661fe1844ab
AhmadBilalAslam <ahmadbilal07@live.com>
2019-04-25 21:33:44 +0500

    BC integrated

--- app.js
+++ app.js
@@ -59,4 +59,11 @@ app.use(function(err, req, res, next) {
 const port = 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 */
+
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 module.exports = app;
+
